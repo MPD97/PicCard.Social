d0707d089f1bd0d9f9095bb46db74b9d476e57fc
MPD97 <mateuszpracadom@gmail.com>
2022-12-23 11:30:14 +0100

    basic api

---
+++ src/PicCard.Social.Api/Services/IDateTimeProvider.cs
@@ -0,0 +1,12 @@
+﻿namespace PicCard.Social.Api.Services
+{
+    public interface IDateTimeProvider
+    {
+        public DateTime Now { get; }
+    }
+
+    public class DateTimeProvider : IDateTimeProvider
+    {
+        public DateTime Now => DateTime.Now;
+    }
+}
